02ffab3a7ad665f991c5be82f0e6a8d91ce40f2c
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-06-21 09:43:33 +0300

    primitive variables vs  objects, values vs refferences

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function createCircle(radius){
+// factory functuion
+/* function createCircle(radius){
     return {
         radius,
         draw: function(){
@@ -6,7 +7,56 @@ function createCircle(radius){
         }
     }
 
+} 
+const circle = createCircle(1);
+//circle.draw();
+*/
+
+//constructor function
+/* function Circle(radius){
+    this.rad = radius,
+    this.draw = function(){
+        console.log('draw ' + this.rad + ' radius');
+    }
 }
 
-const circle = createCircle(1);
-circle.draw();
+const circle2 = new Circle(1);
+circle2.draw(); */
+
+//string code?
+/* const Circle1 = new Function('radius', `
+
+his.rad = radius,
+    this.draw = function(){
+        console.log('draw ' + this.rad + ' radius');
+    }
+`);
+
+const circle3 = new Circle1(3);
+circle3.draw(); */
+
+
+
+// values vs refferences in primitives vs objects
+/* let x = 10;
+let y = x;
+x = 20;
+console.log('x =', x, ' y =', y);
+
+let a = {value: 10};
+let b = a;
+a.value = 20;
+console.log('a =', a.value, ' b =', b.value); */
+
+/* let num = 10;
+function increase(x){ return ++x };
+let newNum = increase(num);
+console.log(newNum); */
+/* let number = 10;
+function increase(number){ number++ };
+increase(number);
+console.log(number); */
+/* let obj = {value: 10};
+function increase(obj){ obj.value++ };
+increase(obj);
+console.log(obj.value); */
