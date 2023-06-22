db7be8e0e46aa08df8e18960292c7648f3eb5789
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-06-22 07:49:55 +0300

    getting and setting privat variables

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ for (let key in circle3){
 } */
 
 //privat vs public properties and methods
-function Circle(radius){
+/* function Circle(radius){
     this.rad = radius;
     //this.defLocation = {x:0, y:0};
     let defLocation = {x:0, y:0};
@@ -102,7 +102,48 @@ function Circle(radius){
         console.log('draw ' + this.rad + ' radius');
     }
 }
-
-const circle4 = new Circle(10);
 //console.log(circle4.defLocation);
 //circle4.computeOptimalLocation(1);
+const circle4 = new Circle(10); */
+
+
+//read privat properties
+//variant 1
+/* function Circle1(radius){
+    this.rad = radius;
+    let defLocation = {x:1, y:1};
+    this.getDefLocation = function(){
+        return defLocation;
+    }
+    this.draw = function(){
+        console.log('draw ' + this.rad + ' radius');
+    }
+}
+
+const circle5 = new Circle1(10);
+console.log(circle5.getDefLocation()); */
+
+//variant 2
+function Circle2(radius){
+    this.rad = radius;
+    let defLocation = { x: 0, y: 0 };
+    this.draw = function(){
+        console.log('draw circle with' + this.rad + ' radius at x=', defLocation.x, ',y =', defLocation.y, 'location');
+    }
+    Object.defineProperty(this, 'defLocation', {
+        get: function(){return defLocation},
+        set: function(value){
+            if(!value.x || !value.y)
+                throw new Error('invalid locatin');
+            
+            defLocation = value;
+        }
+    });
+}
+
+const circle6 = new Circle2(10);
+circle6.draw();
+console.log('location before assigment', circle6.defLocation);
+circle6.defLocation = 1;
+console.log('location after assigment', circle6.defLocation);
+circle6.draw();
