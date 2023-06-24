eb18484b0d321c64385a04edbd8ac0e0f0aae519
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-06-24 11:44:07 +0300

    do method chaninin with return this

--- objects.js
+++ objects.js
@@ -2,12 +2,20 @@ class User {
     constructor(email, name){
         this.email = email;
         this.name = name;
+        this.score = 0;
     }
     login(){
         console.log(this.email, 'just logged in');
+        return this;
     }
     logout(){
         console.log(this.email, 'just logged out');
+        return this;
+    }
+    updateScore(){
+        this.score++;
+        console.log(this.email, 'score is now', this.score);
+        return this;
     }
 }
 
@@ -15,5 +23,6 @@ const user1 = new User('slav@gmail', 'Slav');
 const user2 = new User('pit@gmail', 'Pit');
 
 console.log(user1.name, user2.name);
-user1.login();
-user2.logout();
+console.log(user1.score);
+user1.login().updateScore().updateScore().updateScore().logout();
+console.log(user1.score);
