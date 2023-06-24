1a26186e22033ce5cb2714cc5a368cb1b8dc7f67
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-06-24 15:06:16 +0300

    upd

--- objects.js
+++ objects.js
@@ -19,10 +19,26 @@ class User {
     }
 }
 
+
+class Admin extends User {
+    deleteUser(user){
+        console.log('deleteing user', user.email);
+        users = users.filter(u => u.email != user.email);
+    }
+}
+
 const user1 = new User('slav@gmail', 'Slav');
 const user2 = new User('pit@gmail', 'Pit');
+const admin = new Admin('admin@gmail', 'Den')
 
-console.log(user1.name, user2.name);
+/* console.log(user1.name, user2.name);
 console.log(user1.score);
 user1.login().updateScore().updateScore().updateScore().logout();
 console.log(user1.score);
+ */
+
+let users = [user1, user2, admin];
+
+/* console.log('users', users.forEach(user=>user.name));
+admin.deleteUser(user2);
+console.log('users', users.forEach(user=>user.name)); */
